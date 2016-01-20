4355a94226eeb95c705259ab3b70226edc8311d5
Roman Bobin <roman.bobin@gmail.com>
2016-01-20 18:59:19 +0100

    Added AnyValueProcessorSpec

---
+++ src/test/scala/com/github/rbobin/playjsonmatch/processors/AnyValueProcessorSpec.scala
@@ -0,0 +1,31 @@
+package com.github.rbobin.playjsonmatch.processors
+
+import com.github.rbobin.playjsonmatch.{MatchSuccess, MatchSkip, UnitSpec, MatchError}
+import AnyValueProcessor._
+import play.api.libs.json._
+
+class AnyValueProcessorSpec extends UnitSpec {
+
+  val pattern = AnyValueProcessor.patternString
+
+  "match" should "be skipped with irrelevant patterns" in {
+    val maybeJsValue = Some(JsNull)
+
+    process(null, maybeJsValue) shouldBe MatchSkip
+    process("", maybeJsValue)   shouldBe MatchSkip
+    process(".", maybeJsValue)  shouldBe MatchSkip
+    process("**", maybeJsValue) shouldBe MatchSkip
+  }
+
+  it should "succeed with relevant pattern and any value" in {
+    process(pattern, Some(JsNull))          shouldBe a [MatchSuccess]
+    process(pattern, Some(JsString("")))    shouldBe a [MatchSuccess]
+    process(pattern, Some(JsNumber(1)))     shouldBe a [MatchSuccess]
+    process(pattern, Some(JsArray(Seq())))  shouldBe a [MatchSuccess]
+    process(pattern, Some(JsObject(Seq()))) shouldBe a [MatchSuccess]
+  }
+
+  it should "fail with relevant pattern and None" in {
+    process(pattern, None) shouldBe a [MatchError]
+  }
+}
